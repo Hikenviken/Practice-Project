5ccfb1a3250329ed33a53a8c5c36616aaea2ffbc
Hikeniken <rotmghikenviken@gmail.com>
2019-10-01 20:05:22 -0500

    add fake handle for about page

--- Index.js
+++ Index.js
@@ -1,16 +1,33 @@
 import {Header, Nav, Main, Footer} from "./components";
 
 const state = {
-  Home: {
+  home: {
     heading: "Home Page"
   },
     About: {
       heading: "About Page"
     }
-}
+};
+function render(st = state.home) {
 document.querySelector("#root").innerHTML = `
-${Header(state.Home.heading)}
+${Header(st.heading)}
 ${Nav()}
 ${Main()}
 ${Footer()}
 `;
+}
+render();
+
+const aboutLink = document.querySelector("#about");
+
+aboutLink.addEventListener("click", function(event){
+event.preventDefault();
+render(state[event.target.textContent]);
+
+})
+// const aboutText =
+// console.log("about text is:" , aboutText);
+// console.log(state[aboutText]);
+
+//   console.log("Get your filthy cursor off me")
+//
